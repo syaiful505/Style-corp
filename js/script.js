37c88880d39f84331aa75a81c2b3f802be25f28e
syaiful505 <syaifulstyle505@gmail.com>
2021-01-31 19:29:54 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const sideNav = document.querySelectorAll('.sidenav'); //manggil dg cara dom conts variabel sideNav tangkap elemen dokumen.query javscrip cariin elemen nama selector kelas sidenav
+    M.Sidenav.init(sideNav); //kalau sudah panggil M punya materialize  panggil Sidenav lalu di init elemen sideNav
+    const slider = document.querySelectorAll('.slider');
+    M.Slider.init(slider, {
+        indicators: false, //indikatornya tidak ada kasih koma kalau mau ditambahin lagi
+        height: 500, //tingginya
+        transition: 600, //efek transisinya tulisan muncul
+        interval: 3000 //efek ganti gambar jika sudah tdk ad opsi tdk usah pakai koma
+    });
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax)
+const materialbox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialbox);
