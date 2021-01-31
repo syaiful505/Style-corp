9500e57cdece20023f2b21b9b73902e82ed5b455
syaiful505 <syaifulstyle505@gmail.com>
2021-01-31 20:50:54 +0700

    scrollspy

--- js/script.js
+++ js/script.js
@@ -11,3 +11,8 @@ const parallax = document.querySelectorAll('.parallax');
 M.Parallax.init(parallax)
 const materialbox = document.querySelectorAll('.materialboxed');
 M.Materialbox.init(materialbox);
+
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+    scrollOffset: 50
+});
